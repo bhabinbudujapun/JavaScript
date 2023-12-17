9b8958465caa337e8493fb320a4932f4a8b6dcd6
bhabinpun <poon.bhabin6332@gmail.com>
2023-12-17 13:15:56 +0545

    Basic-JsLoops

---
+++ 02-JsLoops.js
@@ -0,0 +1,20 @@
+// LOOPS
+// FOR LOOP
+const contries = ["Nepal", "India", "Indonesia", "United states"];
+for (let i = 0; i < contries.length; i++) {
+  //   console.log(contries[i]);
+}
+
+// WHILE
+var i = 0;
+while (i < contries.length) {
+  //   console.log(contries[i]);
+  i++;
+}
+
+// DO-WHILE
+i = 0;
+do {
+  console.log(contries[i]);
+  i++;
+} while (i < contries.length);
