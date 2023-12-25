cabd12b5f587385404de7c3119db00ae3c975867
bhabinpun <poon.bhabin6332@gmail.com>
2023-12-25 16:21:40 +0545

    Listing Cource ascending and descending order

--- 08-Project-ListCourses/javascript/ScriptListCourse.js
+++ 08-Project-ListCourses/javascript/ScriptListCourse.js
@@ -1,1 +1,68 @@
-console.log("Hello world!");
+var courses = [
+  {
+    name: "HTML",
+    price: "3.99",
+  },
+  {
+    name: "CSS",
+    price: "5.99",
+  },
+  {
+    name: "Js",
+    price: "7.99",
+  },
+  {
+    name: "React",
+    price: "9.99",
+  },
+  {
+    name: "Angular",
+    price: "9.99",
+  },
+  {
+    name: "Django",
+    price: "6.99",
+  },
+];
+
+function generateCourseList() {
+  const ul = document.querySelector(".list-group");
+  ul.innerHTML = "";
+  courses.forEach((course) => {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+
+    const i = document.createElement("i");
+    i.classList.add("fa-solid", "fa-hand-point-right");
+
+    const name = document.createTextNode(" " + course.name);
+
+    const span = document.createElement("span");
+    span.classList.add("float-end");
+    const price = document.createTextNode("$ " + course.price);
+    span.appendChild(price);
+
+    li.appendChild(i);
+    li.appendChild(name);
+    li.appendChild(span);
+
+    ul.appendChild(li);
+  });
+}
+
+// generateCourseList();
+window.addEventListener("load", generateCourseList);
+
+const button = document.querySelector(".btn-sort");
+button.addEventListener("click", () => {
+  courses.sort((a, b) => a.price - b.price);
+  generateCourseList();
+});
+
+const dbutton = document.querySelector(".btn-dsort");
+dbutton.addEventListener("click", () => {
+  courses = courses
+    .map((course) => ({ ...course, price: parseFloat(course.price) }))
+    .reverse();
+  generateCourseList();
+});
