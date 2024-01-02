9705a302cda54c2cd289e022b0b43a9b649f2ae2
bhabinpun <poon.bhabin6332@gmail.com>
2024-01-02 13:02:05 +0545

    Memory Game - completed

--- 09-Project-MemoryGame/javascript/ScriptMemoryGame.js
+++ 09-Project-MemoryGame/javascript/ScriptMemoryGame.js
@@ -1,10 +1,49 @@
 const cards = document.querySelectorAll(".card");
-console.log(cards);
 
 cards.forEach((card) => card.addEventListener("click", flip));
 
+//Variables
+var isflip = false;
+var firstCard;
+var secondCard;
+
 function flip() {
   this.classList.add("flip");
+  if (!isflip) {
+    isflip = true;
+    firstCard = this;
+  } else {
+    secondCard = this;
+    checkIt();
+  }
+}
+
+function checkIt() {
+  if (firstCard.dataset.image === secondCard.dataset.image) {
+    success();
+  } else {
+    fail();
+  }
+}
+
+function success() {
+  firstCard.removeEventListener("click", flip);
+  secondCard.removeEventListener("click", flip);
+  resetValue();
+}
+
+function fail() {
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetValue();
+  }, 1000);
+}
+
+function resetValue() {
+  isflip = false;
+  firstCard = null;
+  secondCard = null;
 }
 
 (function shuffle() {
