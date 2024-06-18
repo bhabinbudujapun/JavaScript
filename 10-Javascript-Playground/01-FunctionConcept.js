6aba4f61321b7ba518720b2b73783c1f2f56c6fc
Bhabin <poon.bhabin6332@gmail.com>
2024-06-18 08:33:30 +0545

    Function and Object Concept, initial commit

---
+++ 10-Javascript-Playground/01-FunctionConcept.js
@@ -0,0 +1,3 @@
+// In javascript, function are call as first-class-function
+// which means function are treated as variable where function
+// are pass as argument as well as receive or assign on variable
