89c77ca7e68ea455b61cac93776ae9837b268857
bhabinpun <poon.bhabin6332@gmail.com>
2024-06-27 14:36:18 +0545

    tv screen or background color changer project in javascript

---
+++ 13-Simple-Projects/02-Bg-Changer/index.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll("#selectedColor");
+  buttons.forEach((element) =>
+    element.addEventListener("click", handleOperation)
+  );
+
+  function handleOperation(event) {
+    const tvScreen = document.getElementById("tvScreen");
+    let tvScreenClass = Array.from(tvScreen.classList);
+    tvScreenClass = tvScreenClass.filter(
+      (element) => !element.startsWith("bg")
+    );
+
+    const selectedClass = Array.from(event.target.classList);
+    const styleName = selectedClass.find((element) => element.startsWith("bg"));
+    tvScreenClass.push(styleName);
+    tvScreen.className = tvScreenClass.join(" ");
+  }
+});
