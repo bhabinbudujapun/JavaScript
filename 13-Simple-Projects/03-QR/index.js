fde293ba496b0aa6cb22b79621b5105d394ade81
bhabinpun <poon.bhabin6332@gmail.com>
2024-06-28 13:34:31 +0545

    update ui and writing some js to get data or QR

---
+++ 13-Simple-Projects/03-QR/index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("formData");
+  const inputData = document.getElementById("textInput");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    if (inputData.value === "") {
+      alert("Could you please, type some data");
+    } else {
+      const qrImg = document.getElementById("qrImg");
+      const URL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${inputData.value}`;
+      qrImg.src = URL;
+      qrImg.style.display = "block";
+    }
+  });
+});
