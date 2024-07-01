5129d7c861bfd89df1b09a13db51c9c4e17e6fcb
bhabinpun <poon.bhabin6332@gmail.com>
2024-07-01 13:48:27 +0545

    Simple project which generate jokes

---
+++ 13-Simple-Projects/05-Joke-Generator/index.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const generateJoke = document.getElementById("generateJoke");
+
+  async function handleOperation() {
+    try {
+      generateJoke.innerText = "Fetching Joke ...";
+      const URL = "https://official-joke-api.appspot.com/random_joke";
+      const response = await fetch(URL);
+
+      if (!response.ok) {
+        throw new Error("Network Response Was Not OK");
+      }
+
+      const jokeData = await response.json();
+      //   console.log("Joke setup:", jokeData.setup);
+      //   console.log("Joke punchline:", jokeData.punchline);
+
+      const paragraphJoke = document.getElementById("paragraphJoke");
+      generateJoke.innerText = "Get Another Joke";
+      paragraphJoke.innerHTML = `<span> ${jokeData.setup} <br> ${jokeData.punchline}`;
+    } catch (error) {
+      console.error("Something Went Wrong:", error);
+    }
+  }
+
+  generateJoke.addEventListener("click", handleOperation);
+});
