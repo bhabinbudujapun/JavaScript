8c1b0a4baf127b4827ffcbde3b4674f24f667258
bhabinpun <poon.bhabin6332@gmail.com>
2024-07-03 12:11:01 +0545

    complete accordion project

--- 13-Simple-Projects/06-Accordion/index.js
+++ 13-Simple-Projects/06-Accordion/index.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const accordion = document.querySelectorAll(".accordion-header");
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
 
-  for (let index = 0; index < accordion.length; index++) {
-    accordion[index].addEventListener("click", () => {
-        accordion[index].nextElementSibling.classList.toggle("hidden");
+  //Extracting and Adding click event of HEADER of accordion
+  for (let index = 0; index < accordionHeaders.length; index++) {
+    const element = accordionHeaders[index];
+
+    element.addEventListener("click", () => {
+      const accordionContent = element.nextElementSibling;
+
+      //Seting and Removing the '.active' class on each elements
+      if (accordionContent.classList.contains("active")) {
+        accordionContent.classList.remove("active");
+        accordionContent.style.maxHeight = null;
+      } else {
+        accordionContent.classList.add("active");
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+      }
     });
-}
+  }
 });
