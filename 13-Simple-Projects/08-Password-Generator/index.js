11e7930d7db7bd113da7197a3dfef80453a6b0b7
bhabinpun <poon.bhabin6332@gmail.com>
2024-07-07 21:37:42 +0545

    logic portion of project

--- 13-Simple-Projects/08-Password-Generator/index.js
+++ 13-Simple-Projects/08-Password-Generator/index.js
@@ -1,3 +1,29 @@
-function updatePassword(value) {
-  document.getElementById("currentPassword").textContent = value;
-}
+document.addEventListener("DOMContentLoaded", () => {
+  // GENERATE RANDOM SYMBOL/CHARACTER ACCORDING TO USER PROVIDED INFORMATION
+  //   randomCharacters("abcdefghijklmnopqrstuvwxyz", 26, 1);
+  //   randomCharacters("abcdefghijklmnopqrstuvwxyz".toUpperCase(), 26, 1);
+  //   randomCharacters("0123456789", 10, 1);
+  //   randomCharacters("!@#$%^&*()_+}{?/>.<,\\'", 21, 1);
+
+  function randomCharacters(char, totalChar, length) {
+    let result = [];
+    for (let index = 0; index < length; index++) {
+      result.push(char[Math.floor(Math.random() * totalChar)]);
+    }
+    console.log(result);
+  }
+
+  //SELECT DIFFERENT PASSWORD RANGE
+  const passwordRange = document.getElementById("passwordRange");
+  const curPasswordRange = document.getElementById("curPasswordRange");
+
+  passwordRange.addEventListener("input", (event) => {
+    curPasswordRange.textContent = event.target.value;
+  });
+});
+
+// const textToCopy = "Hello, world!";
+// navigator.clipboard
+//   .writeText(textToCopy)
+//   .then(() => console.log("Text copied successfully"))
+//   .catch((err) => console.error("Error copying text:", err));
